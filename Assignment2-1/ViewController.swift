2af25c647871ef6fdaf4624e0cad2ed9dd0d8372
Tony <macuser@macs-macbook.local>
2016-07-19 18:25:23 -0500

    Added comments to some of the methods.

--- Assignment2-1/ViewController.swift
+++ Assignment2-1/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Tony on 6/28/16.
 //  Copyright © 2016 Anthony Cavallo. All rights reserved.
-//
+//  This is just a simple binary calculator game, created to supplement an instructional document for English 3130: Technical Writing
 
 import UIKit
 
@@ -29,6 +29,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    //Every time a switch is flipped, regardless of which one, we need to recalculate the binary string.
     @IBAction func switchFlipped(sender: UISwitch) {
         binaryStringConversion()
     }
@@ -43,6 +44,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var s7: UISwitch!
     @IBOutlet weak var s8: UISwitch!
     
+    //Check the answer to see if what the user entered, through switches, is correct.
     @IBAction func checkAnswer(sender: UIButton) {
         var x: UInt32 = 0
         if s0.on { x += 1 }
@@ -62,6 +64,7 @@ class ViewController: UIViewController {
         }
     }
     
+    //Generate a random number, then turn each switch off.
     @IBAction func newNumber(sender: UIButton) {
         randomNum = arc4random() % 512
         numberLabel.text = String(format: "Convert %i to binary", randomNum)
@@ -79,17 +82,18 @@ class ViewController: UIViewController {
         binaryStringConversion()
     }
 
+    //Change the binary string label, based on whether the switch is on or off for that index.
     func binaryStringConversion() {
         var zero = 0; var one = 0; var two = 0; var three = 0; var four = 0; var five = 0; var six = 0
         var seven = 0; var eight = 0
         
-        if s0.on { zero = 1 } else { zero = 0 }
-        if s1.on { one = 1 } else { one = 0 }
-        if s2.on { two = 1 } else { two = 0 }
+        if s0.on { zero = 1  } else { zero = 0 }
+        if s1.on { one = 1   } else { one = 0 }
+        if s2.on { two = 1   } else { two = 0 }
         if s3.on { three = 1 } else { three = 0 }
-        if s4.on { four = 1 } else { four = 0 }
-        if s5.on { five = 1 } else { five = 0 }
-        if s6.on { six = 1 } else { six = 0 }
+        if s4.on { four = 1  } else { four = 0 }
+        if s5.on { five = 1  } else { five = 0 }
+        if s6.on { six = 1   } else { six = 0 }
         if s7.on { seven = 1 } else { seven = 0 }
         if s8.on { eight = 1 } else { eight = 0 }
         
